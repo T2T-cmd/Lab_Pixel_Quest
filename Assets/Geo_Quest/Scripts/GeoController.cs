2d21c6331c6aeddb174419312840d4912cf6fa31
T2T-cmd <20alansanchez09@gmail.com>
2025-07-09 12:10:45 -0400

    Almost done with the assigement

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Wolrd");
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            float xInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2 (xInput, rb.velocity.y);
+            {
+                rb.velocity = new Vector2(-1, rb.velocity.y);
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -1, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rb.velocity = new Vector2(-1, rb.velocity.y);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rb.velocity = new Vector2(-1, rb.velocity.y);
+            }
+            rb = GetComponent<Rigidbody2D>();
+            {
+
+            }
+        }
+
+    }
+
+}
+
+
