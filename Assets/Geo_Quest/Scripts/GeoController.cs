e82afa8c6c83de70e108c2c703ee9429ea331efe
T2T-cmd <20alansanchez09@gmail.com>
2025-07-10 10:37:29 -0400

    finished lesson 4

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GeoController : MonoBehaviour
+     
 {
     private Rigidbody2D rb;
     public int speed = 10;
+    public string nextlevel = "Scene_2";
     // Start is called before the first frame update
     void Start()
     {
@@ -15,39 +18,63 @@ public class GeoController : MonoBehaviour
     }
 
 
-
-
-
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        switch (collision.tag)
         {
-            float xInput = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextlevel);
+                    break;
 
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                transform.position += new Vector3(0, 1, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                transform.position += new Vector3(0, -1, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                rb.velocity = new Vector2(-1, rb.velocity.y);
-            }
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                rb.velocity = new Vector2(-1, rb.velocity.y);
-            }
-              
-           
-        }   
+
+
+                }
+
+        }
+    }
+                // Update is called once per frame
+
+   
+
+    private void Update()
+    {
         
+            
+    float xInput = Input.GetAxis("Horizontal");
+    rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+    if (Input.GetKeyDown(KeyCode.W))
+    {
+        transform.position += new Vector3(0, 1, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.S))
+    {
+        transform.position += new Vector3(0, -1, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.A))
+    {
+        rb.velocity = new Vector2(-1, rb.velocity.y);
+    }
+    if (Input.GetKeyDown(KeyCode.D))
+    {
+        rb.velocity = new Vector2(-1, rb.velocity.y);
     }
 
+
+            
+
+        
+
+    }
 }
+
+
 
 
